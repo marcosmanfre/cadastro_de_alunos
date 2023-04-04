9fdd72d3bd876ce3ab154713f00d080018bacc57
marcosmanfre <marcosmanfre@hotmail.com>
2023-04-04 08:32:57 -0300

    register

--- register/apps.py
+++ register/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CadastroConfig(AppConfig):
+class RegisterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cadastro'
+    name = 'register'
