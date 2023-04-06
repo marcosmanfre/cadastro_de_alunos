2d159db25761ad3ed4015c5b117aed9d7f014bd3
marcosmanfre <marcosmanfre@hotmail.com>
2023-04-06 09:16:39 -0300

    crud

--- register/models.py
+++ register/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+
 class Aluno(models.Model):
-    registro_aluno = models.PositiveIntegerField()
+    registro_aluno = models.PositiveIntegerField(null=False)
     nome = models.CharField(max_length=50)
     sobrenome = models.CharField(max_length=50)
     secao = models.CharField(max_length=50)
 
+
 def __str__(self):
-    return f'Aluno: {self.nome} {self.sobrenome}'
+    return self.aluno
--- register/views.py
+++ register/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+from .models import Aluno
+
 
 def home(request):
-    return render(request, 'register/pages/home.html')
-    
+    alunos = Aluno.objects.all()
+
+    context = {
+        'alunos': alunos,
+    }
+    return render(request, 'register/pages/home.html',context)
+
