45df336ae9220e12a6dc1575250176a195d49df2
marcosmanfre <marcosmanfre@hotmail.com>
2023-04-07 16:11:11 -0300

    view add

--- register/views.py
+++ register/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from register.models import Aluno
 
 from .models import Aluno
 
@@ -11,3 +12,20 @@ def home(request):
     }
     return render(request, 'register/pages/home.html',context)
 
+
+def adicionar(request):
+    if request.method == "POST":
+        registro = request.POST.get('registro')
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        secao = request.POST.get('secao')
+
+        alunos = Aluno(
+            registro = registro,
+            nome = nome,
+            sobrenome = sobrenome,
+            secao = secao
+
+        )
+        alunos.save()
+        return redirect('register/pages/home.html')
