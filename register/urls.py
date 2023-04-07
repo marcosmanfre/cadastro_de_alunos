616ac62d7a3938f002e33f26c89d114818771b7e
marcosmanfre <marcosmanfre@hotmail.com>
2023-04-07 16:15:54 -0300

    url

--- register/urls.py
+++ register/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import include, path
 from .views import home
 
+from . import views
+
 urlpatterns = [
     path('', home),
+    path('adicionar', views.adicionar, name="adicionar")
 
 
 ]
