da34f91ea146a6922cfc210390e2b8d1f2ff7157
marcosmanfre <marcosmanfre@hotmail.com>
2023-04-08 16:40:49 -0300

    edit

--- project/urls.py
+++ project/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', include('register.urls'))
+    path('', include('register.urls'))
     
 
 ]
